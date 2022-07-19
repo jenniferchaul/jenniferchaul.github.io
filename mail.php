e0f63d2026eec8c10b02818b29ce9ac200a3c01e
Jennifer <chaul.jennifer@gmail.com>
2022-07-19 13:45:12 +0200

    setting up a button to go to the top of the page

--- mail.php
+++ mail.php
@@ -1,45 +1,17 @@
 <?php
-    require_once(__DIR__.'/vendor/autoload.php');
-    use \Mailjet\Resources;
+//get data from form  
 
-    define('API_USER', '9908c56b6ddedabdfe758536cec22523');
-    define('API_LOGIN', '09551f87013fa2b5b3bd0ca6a3ccb522');
-    $mj = new \Mailjet\Client(API_USER, API_LOGIN,true,['version' => 'v3.1']);
-    
-    if (!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])) {
-        $surname = htmlspecialchars($_POST['surname']);
-        $firstname = htmlspecialchars($_POST['firstname']);
-        $email = htmlspecialchars($_POST['email']);
-        $message =htmlspecialchars($_POST['message']);
-
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $body = [
-                'Messages' => [
-                    [
-                        'From' => [
-                            'Email' => "chaul.jennifer@gmail.com",
-                            'Name' => "Mailjet Pilot"
-                        ],
-                        'To' => [
-                            [
-                                'Email' => "chaul.jennifer@gmail.com",
-                                'Name' => "Mailjet Pilot"
-                            ]
-                        ],
-                        'Subject' => "Your email",
-                        'TextPart' => "$email, $message",
-                    ]
-                ]
-            ];
-
-            $response = $mj->post(Resources::$Email, ['body' => $body]);
-            $response->success();
-            echo "Message envoyé avec succès !";
-        } else {
-            echo "Email non valide";
-        }
-    }else{
-        header('Location:index.html');
-        die();
-    }
-
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "chaul.jennifer@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: poussin71@hotmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function toggleMenu() {
     }
 }
 
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
+
